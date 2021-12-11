f4d1966791dfe02f3c51e33ff1b63563331e8329
florscaglione <florencia.scaglione@hotmail.com>
2021-12-11 01:12:33 +0000

    creando endpoints

--- src/main.py
+++ src/main.py
@@ -8,7 +8,7 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, User, Character, Planet, FavoriteCharacter, FavoritePlanet
 #from models import Person
 
 app = Flask(__name__)
@@ -30,16 +30,89 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
-@app.route('/user', methods=['GET'])
-def handle_hello():
+# Endpoint para CREAR VARIOS USUARIOS (y no tener que crear uno a uno en el flask admin):
+@app.route('/create-users', methods=['GET'])
+def list_of_users():
 
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
+    data = {
+        "users": [
+            {
+                "email": "usuario1@gmail.com",
+                "password": "1234",
+                "is_active": True            
+            },
+            {
+                "email": "usuario2@gmail.com",
+                "password": "1994",
+                "is_active": True              
+            },
+            {
+                "email": "usuario3@gmail.com",
+                "password": "0000",
+                "is_active": True              
+            },
+            {
+                "email": "usuario4@gmail.com",
+                "password": "8753",
+                "is_active": True              }
+        ],
     }
 
-    return jsonify(response_body), 200
+    for user in data['users']:
+        new_user = User(email = user['email'], password = user['password'], is_active = user['is_active'])
+        db.session.add(new_user)
+
+    db.session.commit()
+    
+    return jsonify({"msg": "Users loaded"})
+
+# Obtener la lista de todos los usuarios:
+@app.route('/users', methods=['GET'])
+def get_all_users():
+    users = User.query.all()  # busco en la BBDD todos los usuarios
+
+    users_serialized = list(map(lambda user: user.serialize(), users)) # del array de usuarios (users), consigo cada usuario serializado
+
+    return jsonify(users_serialized), 200 
+
+# Obtener la lista de todos los favoritos de un usuario: AMBAS OPCIONES DEVUELVEN SIEMPRE "[], 200" ???
+@app.route('/users/<int:user_id>/favorites', methods=['GET'])
+def get_user_favorites(user_id):
+    """ user = User.query.get(user_id)
+
+    favs_characters = FavoriteCharacter.query.filter_by(user_id=user_id) # busco en la tabla FavoriteCharacter los personajes favoritos del usuario (mediante el user_id)
+
+    favs_planets = FavoritePlanet.query.filter_by(user_id=user_id)
+
+    favorites = []
+
+    for character in favs_characters:
+        favorites.append(character.serialize())  
+
+    for planet in favs_planets:
+        favorites.append(planet.serialize())          
+
+    return jsonify(favorites), 200  """
+
+    user = User.query.get(user_id)
+
+    favorites_characters = FavoriteCharacter.query.filter_by(user_id=user_id) 
+
+    favs_characters = list(map(lambda fav_character: Character.query.get(favoriteCharacter.character_id).serialize(), favorites_characters)) # para cada inscripción, busco el id de usuario para de ahí sacar el propio usuario
+
+    return jsonify(favs_characters), 200    
+
+# Añadir un planeta favorito a un usuario: NO FUNCIONa
+@app.route('/favorite/planet/<int:planet_id>', methods=['POST'])
+def add_favorite_planet(planet_id):
+    planet = Planet.query.get(planet_id)
+    user = User.query.get(1)
+    user.favoritePlanets.append(planet_id)
+    db.session.commit()
+    return jsonify(planet.serialize()), 200
+
 
 # this only runs if `$ python src/main.py` is executed
-if __name__ == '__main__':
-    PORT = int(os.environ.get('PORT', 3000))
-    app.run(host='0.0.0.0', port=PORT, debug=False)
+# if __name__ == '__main__':
+#     PORT = int(os.environ.get('PORT', 3000))
+#     app.run(host='0.0.0.0', port=PORT, debug=False)
--- src/models.py
+++ src/models.py
@@ -3,17 +3,103 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer(), primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
 
+    favoriteCharacters = db.relationship('FavoriteCharacter', backref=db.backref('user', lazy=True))
+
+    favoritePlanets = db.relationship('FavoritePlanet', backref=db.backref('user', lazy=True))
+
     def __repr__(self):
-        return '<User %r>' % self.username
+        return '<User %r>' % self.email
 
     def serialize(self):
         return {
             "id": self.id,
             "email": self.email,
             # do not serialize the password, its a security breach
+        }
+
+class Character(db.Model):
+    id = db.Column(db.Integer(), primary_key=True)
+    name = db.Column(db.String(120), unique=True, nullable=False)
+    height = db.Column(db.Integer())
+    hair_color = db.Column(db.String(50))
+    skin_color = db.Column(db.String(50)) 
+    eye_color = db.Column(db.String(50))
+    gender = db.Column(db.String(50))
+    birth_year = db.Column(db.String(50))
+
+    favoriteCharacters = db.relationship('FavoriteCharacter', backref=db.backref('character', lazy=True))
+
+
+    def __repr__(self):
+        return '<Character %r>' % self.name
+
+    def serialize(self):
+        return {
+            "name": self.name,
+            "height": self.height,
+            "hair_color": self.hair_color,
+            "skin_color": self.skin_color,
+            "eye_color": self.eye_color,
+            "gender": self.gender,
+            "birth_year": self.birth_year    
+        }  
+
+class Planet(db.Model):
+    id = db.Column(db.Integer(), primary_key=True)
+    name = db.Column(db.String(120), unique=True, nullable=False)
+    population = db.Column(db.Integer())
+    climate = db.Column(db.String(50))
+    diameter = db.Column(db.Integer()) 
+    terrain = db.Column(db.String(50))
+    surface_water = db.Column(db.Integer())
+
+    favoritePlanets = db.relationship('FavoritePlanet', backref=db.backref('planet', lazy=True))
+
+
+    def __repr__(self):
+        return '<Character %r>' % self.name
+
+    def serialize(self):
+        return {
+            "name": self.name,
+            "population": self.population,
+            "climate": self.climate,
+            "diameter": self.diameter,
+            "terrain": self.terrain,
+            "surface_water": self.surface_water
+        }               
+
+class FavoriteCharacter(db.Model):
+    id = db.Column(db.Integer(), primary_key=True)
+    user_id = db.Column(db.Integer(), db.ForeignKey('user.id', ondelete='CASCADE')) # ondelete: permite eliminar datos de las tablas secundarias automáticamente cuando elimina los datos de la tabla principal
+    character_id = db.Column(db.Integer(), db.ForeignKey('character.id', ondelete='CASCADE'))
+
+    def __repr__(self):
+        return '<FavoriteCharacter %r>' % self.id
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "user_id": self.user_id,
+            "character_id": self.character_id
+        }
+
+class FavoritePlanet(db.Model):
+    id = db.Column(db.Integer(), primary_key=True)
+    user_id = db.Column(db.Integer(), db.ForeignKey('user.id', ondelete='CASCADE')) # ondelete: permite eliminar datos de las tablas secundarias automáticamente cuando elimina los datos de la tabla principal
+    planet_id = db.Column(db.Integer(), db.ForeignKey('planet.id', ondelete='CASCADE'))
+
+    def __repr__(self):
+        return '<FavoritePlanet %r>' % self.id
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "user_id": self.user_id,
+            "planet_id": self.planet_id
         }
